c090d6f75828e2361e0324b4e761eaed108cc9be
pyritepirate <pyritepirate@protonmail.com>
2019-02-18 18:09:01 +0100

    Changes For Pyrite

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xd3, 0x61, 0x1a, 0x6c };
 
 CMessageHeader::CMessageHeader()
 {
